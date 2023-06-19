2905b66bfbab80c147c2095a60f424bbc2a72647
le yagaw <sawlelyagaw@gmail.com>
2023-06-19 23:05:58 +0700

    add draft header

---
+++ src/app/components/MenuItem.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function MenuItem({title,address, Icon}){
+    return(
+        <div>
+             <Link href={address}>
+                <Icon/>
+                 <p>{title}</p>
+            </Link> 
+            
+        </div>
+    )
+}
---
+++ src/app/components/header.jsx
@@ -0,0 +1,16 @@
+import MenuItem from "./MenuItem";
+import {AiFillHome} from "react-icons/ai";
+
+export default function Header(){
+    return(
+        <div>
+            <div className="">
+                <MenuItem title="Home" address="/#" Icon={AiFillHome}/>
+            </div>
+            <div className="">
+
+            </div>
+
+        </div>
+    )
+}
